f26662f246f54bde8c73c6d966f2eddb68d1261b
balamuruganbtech <38473822+balamuruganbtech@users.noreply.github.com>
2020-12-02 15:47:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws"{
     region = "us-east-2"
-    access_key = "AKIAJK4QJWROPYDJ4PKA"
-    secret_key = "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"
+    access_key = "AKIAJK4QJWROPY2DJ4PKA"
+    secret_key = "nmxRthJSM/skc6Y5GEAJtOg3YpNpw4qXqND5JIeWy"
 }
